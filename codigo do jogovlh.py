08f073dc284e4b9cc450d2ae6b508a2f3d86e5d1
Felipe <jfbsilva1202@gmail.com>
2025-05-15 13:22:32 -0300

    adicionando codigo

--- codigo do jogovlh.py
+++ codigo do jogovlh.py
@@ -5,3 +5,13 @@ def print_tabuleiro(tab):
         print(" | ".join(linha))
         print("-" * 5)
     print()
+
+# função para checar vitoria 
+def checar_vitoria(tab, jogador):
+    # Verifica linhas, colunas e diagonais
+    for i in range(3):
+        if all([tab[i][j] == jogador for j in range(3)]) or all([tab[j][i] == jogador for j in range(3)]):
+            return True
+    if all([tab[i][i] == jogador for i in range(3)]) or all([tab[i][2-i] == jogador for i in range(3)]):
+        return True
+    return False
